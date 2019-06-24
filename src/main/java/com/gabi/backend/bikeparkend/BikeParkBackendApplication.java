f37ea69fc98cdb23eeddcfe6428babadb9b550c4
abrudangabi <abrudan_gabi@yahoo.com>
2019-06-24 07:11:43 +0300

    First commit

---
+++ src/main/java/com/gabi/backend/bikeparkend/BikeParkBackendApplication.java
@@ -0,0 +1,12 @@
+package com.gabi.backend.bikeparkend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeParkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeParkBackendApplication.class, args);
+    }
+}
